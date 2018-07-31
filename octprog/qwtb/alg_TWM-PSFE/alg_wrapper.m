46d3dcd634d4c569b0a5d68c58013760877b03b0
smaslan <s.maslan@seznam.cz>
2018-07-31 09:31:01 +0200

    fix: qwtb: TWM-PSFE fixed uncertainty propagation of phase angle
    - uncertainty of the phase angle was incorrectly propagated via the
    algorithm
    - has no effect to anything because PSFE uncertainty estimator covers
    only frequency

--- octprog/qwtb/alg_TWM-PSFE/alg_wrapper.m
+++ octprog/qwtb/alg_TWM-PSFE/alg_wrapper.m
@@ -203,7 +203,7 @@ function dataout = alg_wrapper(datain, calcset)
             u_ph = ph*u_ap;
         else
             % -- tran. type defined, apply correction:
-            [A,ph,u_A,u_ph] = correction_transducer_loading(tab,datain.tr_type.v,f,[], A,ph,A.*adc_gain.u_gain,ph.*adc_phi.u_phi);                
+            [A,ph,u_A,u_ph] = correction_transducer_loading(tab,datain.tr_type.v,f,[], A,ph,A.*adc_gain.u_gain,adc_phi.u_phi);                
         end
         
         if any(isnan(A))
