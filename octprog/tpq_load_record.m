1ff668f959e3992c77e272932237e122d85b92d3
smaslan <smaslan@cmi.cz>
2019-01-18 19:05:40 +0100

    fix: octprog: matlabization of tpq_load_record()

--- octprog/tpq_load_record.m
+++ octprog/tpq_load_record.m
@@ -335,13 +335,11 @@ function [data] = tpq_load_record(header, group_id, repetition_id,data_ofs,data_
       
         % build absolute transducer correction path
         if has_transducers
-            try
-                % absolute path
-                t_file = what(transducer_paths{t});
-                t_file = t_file.path;
-            catch
-                % relative path
-                t_file = [data.meas_folder filesep() transducer_paths{t}];    
+            
+            t_file = [data.meas_folder filesep() transducer_paths{t}];
+            if ~exist(t_file,'file')
+                % possibly absolute path?
+                t_file = transducer_paths{t};
             end             
         else
             t_file = '';
