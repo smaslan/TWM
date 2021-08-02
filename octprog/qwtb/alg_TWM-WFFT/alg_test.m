c21787c405fa19d00a8954c882b7ffb1a59a4a15
smaslan <smaslan@cmi.cz>
2021-08-02 07:59:13 +0200

    fix: TWM-WFFT experimentally modified for direct call of fft functions (lower overhead)

--- octprog/qwtb/alg_TWM-WFFT/alg_test.m
+++ octprog/qwtb/alg_TWM-WFFT/alg_test.m
@@ -9,7 +9,7 @@ function alg_test(calcset) %<<<1
 
     
     % testing mode {0: single test, N >= 1: N repeated tests}:
-    is_full_val = 5000;
+    is_full_val = 0;
     
     % minimum number of repetitions per test setup:
     %  note: if the value is 1 and all quantities passed, the test is done successfully
