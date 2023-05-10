7edaab146e199969ff4213ae2627ad8ad7b8c247
LVNTB1 <smaslan@cmi.cz>
2023-05-10 14:34:24 +0200

    fix: alg: TWM-RATWFFT updated alg_info, for transducer Zbuf correction (was implemented, but not listed in alg_info)

--- octprog/qwtb/alg_TWM-RATWFFT/alg_info.m
+++ octprog/qwtb/alg_TWM-RATWFFT/alg_info.m
@@ -399,6 +399,31 @@ function alginfo = alg_info() %<<<1
     pid = pid + 1;
     [alginfo,pid] = add_ui_pair(alginfo,pid,0);
     
+    % Optional buffer impedance matrix (1D dependence, rows: freqs.)
+    alginfo.inputs(pid).name = 'tr_Zbuf_f';
+    alginfo.inputs(pid).desc = 'Transducer optional output buffer impedance: frequency axis';
+    alginfo.inputs(pid).alternative = 0;
+    alginfo.inputs(pid).optional = 1;
+    alginfo.inputs(pid).parameter = 0;
+    pid = pid + 1;
+    [alginfo,pid] = add_ui_pair(alginfo,pid,0);
+    
+    alginfo.inputs(pid).name = 'tr_Zbuf_Rs';
+    alginfo.inputs(pid).desc = 'Transducer optional output buffer impedance: series resistance';
+    alginfo.inputs(pid).alternative = 0;
+    alginfo.inputs(pid).optional = 1;
+    alginfo.inputs(pid).parameter = 0;
+    pid = pid + 1;
+    [alginfo,pid] = add_ui_pair(alginfo,pid,0);
+    
+    alginfo.inputs(pid).name = 'tr_Zbuf_Ls';
+    alginfo.inputs(pid).desc = 'Transducer optional output buffer impedance: series inductance';
+    alginfo.inputs(pid).alternative = 0;
+    alginfo.inputs(pid).optional = 1;
+    alginfo.inputs(pid).parameter = 0;
+    pid = pid + 1;
+    [alginfo,pid] = add_ui_pair(alginfo,pid,0);
+    
     % Transducer output terminals series impedance matrix (1D dependence, rows: freqs.)
     alginfo.inputs(pid).name = 'tr_Zca_f';
     alginfo.inputs(pid).desc = 'Transducer terminals series impedance: frequency axis';
